5e3a4c4d856d32ff8dcef279ae7824219ffcc476
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-12-21 19:11:58 -0300

    feat: Add rate limit to requests in any routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,10 @@ import { resolve } from 'path';
 import helmet from 'helmet';
 import cors from 'cors';
 
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
+
 import '~/database';
 import routes from '~/routes';
 
@@ -19,9 +23,23 @@ class App {
     this.server.use(helmet());
     this.server.use(cors({ exposedHeaders: 'total_pages' }));
     this.server.use(express.json());
+
     this.server.use(
       '/public',
       express.static(resolve(__dirname, '..', 'public'))
+    );
+
+    this.server.use(
+      new RateLimit({
+        store: new RateLimitRedis({
+          client: redis.createClient({
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
+          }),
+        }),
+        windowMs: 1000 * 60 * 15,
+        max: 100,
+      })
     );
   }
 
